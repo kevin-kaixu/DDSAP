5e10c80932977d5ec1fb71e2d14bd787dde61c34
kalov <kalo@cs.umass.edu>
2016-08-20 12:05:17 -0400

    EG STAR report tex files and EG presentation
    Signed-off-by: kalov <kalo@cs.umass.edu>

---
+++ EG_STAR/MAKEPDF.bat
@@ -0,0 +1,25 @@
+del *.aux
+del *.out
+del *.sav
+del *.log
+del *.brf
+del *.lbl
+del *.bbl
+del *.blg
+
+pdflatex star
+bibtex star
+pdflatex star
+pdflatex star
+
+del *.aux
+del *.out
+del *.sav
+del *.log
+del *.brf
+del *.lbl
+del *.bbl
+del *.blg
+del star.ps
+del star.npg
+del star.bbl
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Data-Driven Shape Analysis and Processing
 SIGGRAPH Asia 2016 course notes
-Vladimir G. Kim, Kai Xu, Qixing Huang, Evangelos Kalogerakis and Niloy Mitra
+Vladimir G. Kim, Kai Xu, Qixing Huang, Niloy Mitra, and Evangelos Kalogerakis
